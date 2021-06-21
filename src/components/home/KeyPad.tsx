b4eef13078af25e4bcce22c46fbf93015653b088
EnzoColinecul <enzocolinecul1997@gmail.com>
2021-06-21 19:29:49 -0300

    Change style in home page and add native packages

--- src/components/home/KeyPad.tsx
+++ src/components/home/KeyPad.tsx
@@ -1,82 +1,90 @@
-import { IonButton, IonButtons, IonCol, IonGrid, IonIcon, IonRow } from '@ionic/react'
-import { callSharp, laptop } from 'ionicons/icons'
+import {
+  IonButton,
+  IonButtons,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonRow
+} from '@ionic/react'
+import { backspace, callSharp } from 'ionicons/icons'
 import React from 'react'
 
 type HomeProps = {
   setInputValue: React.Dispatch<React.SetStateAction<string>>,
+  handleBackspace: () => void,
   handleCall: () => void
 }
 
-const KeyPad = ({ setInputValue, handleCall }: HomeProps) => {
+const KeyPad = ({ setInputValue, handleCall, handleBackspace }: HomeProps) => {
 
   const numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
   return (
-      <>
-        <div className="keypad-row"  >
-          {numbers[0].map(number => (
-            <IonButton
-              expand="full"
-              onClick={() => setInputValue(prevState => prevState + number.toString())}
-              className="keypad-button"
-              key={number}
-            >
-              {number}
-            </IonButton>
-          ))
-          }
-        </div>
-        <div className="keypad-row" >
-          {numbers[1].map(number => (
-            <IonButton
-              expand="full"
-              onClick={() => setInputValue(prevState => prevState + number.toString())}
-              className="keypad-button"
-              key={number}
-            >
-              {number}
-            </IonButton>
-          ))
-          }
-        </div >
-        <div className="keypad-row" >
-          {numbers[2].map(number => (
-            <IonButton
-              expand="full"
-              onClick={() => setInputValue(prevState => prevState + number.toString())}
-              className="keypad-button"
-              key={number}
-            >
-              {number}
-            </IonButton>
-          ))
-          }
-        </div>
-        <div className="keypad-row" >
-          <div className="keypad-button-call-container">
-            <IonButton
-              shape="round"
-              expand="block"
-              color="secondary"
-              size="small"
-              className="keypad-button keypad-button-call-color "
-              onClick={handleCall}
-            >
-              <IonIcon size="extralarge" icon={callSharp} />
-            </IonButton>
-          </div>
+    <>
+      <div className="keypad-row"  >
+        {numbers[0].map(number => (
           <IonButton
             expand="full"
-            onClick={() => setInputValue(prevState => prevState + "0")}
+            onClick={() => setInputValue(prevState => prevState + number.toString())}
             className="keypad-button"
+            key={number}
           >
-            0
+            {number}
           </IonButton>
-          <IonButton className="keypad-button" >
-            <IonIcon icon={laptop} />
+        ))
+        }
+      </div>
+      <div className="keypad-row" >
+        {numbers[1].map(number => (
+          <IonButton
+            expand="full"
+            onClick={() => setInputValue(prevState => prevState + number.toString())}
+            className="keypad-button"
+            key={number}
+          >
+            {number}
+          </IonButton>
+        ))
+        }
+      </div >
+      <div className="keypad-row" >
+        {numbers[2].map(number => (
+          <IonButton
+            expand="full"
+            onClick={() => setInputValue(prevState => prevState + number.toString())}
+            className="keypad-button"
+            key={number}
+          >
+            {number}
+          </IonButton>
+        ))
+        }
+      </div>
+      <div className="keypad-row" >
+        <div className="keypad-button-call-container">
+          <IonButton
+            shape="round"
+            expand="block"
+            color="secondary"
+            size="small"
+            className="keypad-button keypad-button-call-color "
+            onClick={handleCall}
+          >
+            <IonIcon size="extralarge" icon={callSharp} />
           </IonButton>
         </div>
-      </>
+        <IonButton
+          expand="full"
+          onClick={() => setInputValue(prevState => prevState + "0")}
+          className="keypad-button"
+        >
+          0
+        </IonButton>
+        <IonButton onClick={handleBackspace} className="keypad-button" >
+          <IonIcon color="secondary" icon={backspace} />
+        </IonButton>
+      </div>
+    </>
   )
 }
 
